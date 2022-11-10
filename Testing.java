c88bb353358e4e227d98574595fac9aa110a1087
hamannkd24 <117695486+hamannkd24@users.noreply.github.com>
2022-11-09 16:17:34 -0800

    Quick Edit
    Added two variables and tested adding 1 to one of them.

---
+++ Testing.java
@@ -0,0 +1,12 @@
+public class Testing {
+    public static void main(String[] args) {
+        
+        int numberOne = 35;
+        //int numberTwo = 40;
+
+        numberOne ++;
+
+        System.out.println(numberOne);
+
+    }
+}
