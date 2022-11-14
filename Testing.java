747a2af2d78cae7d3c78a9674e39d0d0fdb6dc1b
CoeDexter <coe.dexter@sonomaacademy.org>
2022-11-14 15:50:23 -0800

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -2,11 +2,11 @@ public class Testing {
     public static void main(String[] args) {
         
         int numberOne = 35;
-        //int numberTwo = 40;
+        int numberTwo = 40;
 
         numberOne ++;
 
-        System.out.println(numberOne);
+        System.out.println(numberOne + numberTwo);
 
     }
 }
